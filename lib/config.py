9cb167ca772495e1dbfbd689dae88189d385c28a
CKA <18747836+cccorn@users.noreply.github.com>
2020-01-28 16:53:37 +0800

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -2,9 +2,9 @@ import numpy as np
 
 gpu_ids=[0,1]
 batch_size=8
-epochs=100
+
 lr=0.001
-lr_step=[60,80]
+
 warmup=True
 warmup_step=500
 warmup_factor=0.33333333
@@ -18,7 +18,7 @@ num_anchors=len(anchor_ratios)*len(anchor_scales)
 
 pixel_mean = np.array([[[102.9801, 115.9465, 122.7717]]])
 
-dataset_coco={'dataset':'coco', 'path':'data/coco', 'train_set':'train2017', 'test_set':'val2017'}
-dataset_voc={'dataset':'voc', 'path':'data/voc', 'train_set':'2007_trainval+2012_trainval', 'test_set':'2007_test'}
+dataset_coco={'dataset':'coco', 'path':'data/coco', 'train_set':'train2017', 'test_set':'val2017', 'epochs':100, 'lr_step':[60,80]}
+dataset_voc={'dataset':'voc', 'path':'data/voc', 'train_set':'2007_trainval+2012_trainval', 'test_set':'2007_test', 'epochs':160, 'lr_step':[110,140]}
 
 depth=101
